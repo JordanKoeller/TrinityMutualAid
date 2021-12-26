5a51f8d6c99f1c4314f32747807b93c81e43e1ef
JordanKoeller <jkoeller12@gmail.com>
2021-12-26 16:28:16 -0600

    Finished migrating over to i18n and fixed some styling issues. Now I just need actual i18n content!

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,19 +1,20 @@
 import React, { useMemo, useState } from 'react';
 
 import {
-  Navbar,
-  Container,
-  Nav,
-  Image,
+    Navbar,
+    Container,
+    Nav,
+    Image,
+    NavDropdown,
 } from 'react-bootstrap';
 
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  NavLink as Link,
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    NavLink as Link,
 } from 'react-router-dom';
-import { MediaQuery, useMediaQuery } from '../utilities/hooks';
+import { useMediaQuery, MediaQuery } from '../utilities/hooks';
 import AboutPage from '../pages/About';
 import ContactPage from '../pages/Contact';
 import DonatePage from '../pages/Donate';
@@ -23,82 +24,112 @@ import RequestAidPage from '../pages/RequestAid';
 import ResourcesPage from '../pages/Resources';
 import HomePage from '../pages/Home';
 import FAQsPage from '../pages/Faqs';
+import { useTranslation } from 'react-i18next';
+import { LANGUAGE_MAP } from '../i18n';
 
 
+const CollapsingNavigation: React.FC<{ t: any, i18n: any, setOpen: (v: boolean) => void, }> = ({ i18n, setOpen, t }) => {
+
+    const closeAll = () => {
+        setOpen(false);
+    }
+
+    return <Navbar.Collapse>
+        <Nav style={{ margin: 'auto' }}>
+            <Link className="nav-link" activeClassName="nav-link-active" to="/News" onClick={closeAll}>{t('navbar.News')}</Link>
+            <Link className="nav-link" activeClassName="nav-link-active" to="/RequestAid" onClick={closeAll}>{t('navbar.Request Aid')}</Link>
+            <Link className="nav-link" activeClassName="nav-link-active" to="/Resources" onClick={closeAll}>{t('navbar.Resources')}</Link>
+            <NavDropdown title={t('navbar.About')} menuVariant="dark">
+                <NavDropdown.Item><Link className="nav-link" activeClassName="nav-link-active" to="/About" onClick={closeAll} >{t('navbar.About')}</Link></NavDropdown.Item>
+                <NavDropdown.Item><Link className="nav-link" activeClassName="nav-link-active" to="/FAQs" onClick={closeAll}>{t('navbar.FAQs')}</Link></NavDropdown.Item>
+                <NavDropdown.Item><Link className="nav-link" activeClassName="nav-link-active" to="/Contact" onClick={closeAll}>{t('navbar.Contact')}</Link></NavDropdown.Item>
+            </NavDropdown>
+        </Nav>
+        <LanguageButton i18n={i18n} onSelect={closeAll} />
+    </Navbar.Collapse>
+}
 
 export const TmaNavbar: React.FC = () => {
 
-  const mq = useMediaQuery();
 
-  const [open, setOpen] = useState(false);
+    const mq = useMediaQuery();
+    const { t, i18n } = useTranslation(undefined, { useSuspense: false });
 
-  const link = useMemo(() => <Link
-    className="donate-button"
-    style={{ borderRadius: 0, border: '0px solid #f00' }}
-    to="/Donate"
-    activeClassName="donate-button-active">
-    <b>Donate</b>
-  </Link>, []);
+    const [open, setOpen] = useState(false);
 
-  const collapser = useMemo(() => <Navbar.Collapse>
-    <Nav style={{ margin: 'auto' }}>
-      <Link className="nav-link" activeClassName="nav-link-active" to="/About" onClick={() => setOpen(false)} >About</Link>
-      <Link className="nav-link" activeClassName="nav-link-active" to="/RequestAid" onClick={() => setOpen(false)}>Request Aid</Link>
-      <Link className="nav-link" activeClassName="nav-link-active" to="/News" onClick={() => setOpen(false)}>News</Link>
-      {/* <Link className="nav-link" activeClassName="nav-link-active" to="/Report" onClick={() => setOpen(false)}>Annual Report</Link> */}
-      <Link className="nav-link" activeClassName="nav-link-active" to="/Resources" onClick={() => setOpen(false)}>Resources</Link>
-      <Link className="nav-link" activeClassName="nav-link-active" to="/FAQs" onClick={() => setOpen(false)}>FAQs</Link>
-      <Link className="nav-link" activeClassName="nav-link-active" to="/Contact" onClick={() => setOpen(false)}>Contact</Link>
-    </Nav>
-  </Navbar.Collapse>, [setOpen]);
+    const link = useMemo(() => <Link
+        className="donate-button"
+        style={{ borderRadius: 0, border: '0px solid #f00' }}
+        to="/Donate"
+        activeClassName="donate-button-active">
+        <b>{t('navbar.Donate')}</b>
+    </Link>, [t]);
 
-  return <Navbar bg="navbar" expand="lg" sticky="top" expanded={open}>
-    <Container fluid="md">
-      <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={()=> setOpen(!open)} />
-      <Navbar.Brand as={() => <a href="/Home">
-        <Image src="/tma-logo-banner.png" alt="" id="tma-logo" />
-      </a>}>
-      </Navbar.Brand>
-      {mq <= MediaQuery.MD ? <> {link} {collapser}</> : <>{collapser} {link}</>}
-    </Container>
-  </Navbar>
+    return <Navbar bg="navbar" expand="lg" sticky="top" expanded={open}>
+        <Container fluid="xl">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setOpen(!open)} />
+            <Navbar.Brand as={() => <a href="/Home">
+                <Image src="/tma-logo-banner.png" alt="" id="tma-logo" />
+            </a>}>
+            </Navbar.Brand>
+            {mq <= MediaQuery.MD ? <> {link} <CollapsingNavigation i18n={i18n} t={t} setOpen={setOpen} /></> : <><CollapsingNavigation i18n={i18n} t={t} setOpen={setOpen} /> {link}</>}
+        </Container>
+    </Navbar>
 }
 
 export const TmaRouter: React.FC = () => {
-  return <Router>
-    <TmaNavbar />
-    <Container fluid>
-      <div className="App-header">
-        <Switch>
-          <Route path="/About">
-            <AboutPage />
-          </Route>
-          <Route path="/RequestAid">
-            <RequestAidPage />
-          </Route>
-          <Route path="/Donate">
-            <DonatePage />
-          </Route>
-          <Route path="/Report">
-            <ReportPage />
-          </Route>
-          <Route path="/News">
-            <NewsPage />
-          </Route>
-          <Route path="/Resources">
-            <ResourcesPage />
-          </Route>
-          <Route path="/FAQs">
-            <FAQsPage />
-          </Route>
-          <Route path="/Contact">
-            <ContactPage />
-          </Route>
-          <Route path="">
-            <HomePage />
-          </Route>
-        </Switch>
-      </div>
-    </Container>
-  </Router>
+    return <Router>
+        <TmaNavbar />
+        <Container fluid>
+            <div className="App-header">
+                <Switch>
+                    <Route path="/About">
+                        <AboutPage />
+                    </Route>
+                    <Route path="/RequestAid">
+                        <RequestAidPage />
+                    </Route>
+                    <Route path="/Donate">
+                        <DonatePage />
+                    </Route>
+                    <Route path="/Report">
+                        <ReportPage />
+                    </Route>
+                    <Route path="/News">
+                        <NewsPage />
+                    </Route>
+                    <Route path="/Resources">
+                        <ResourcesPage />
+                    </Route>
+                    <Route path="/FAQs">
+                        <FAQsPage />
+                    </Route>
+                    <Route path="/Contact">
+                        <ContactPage />
+                    </Route>
+                    <Route path="">
+                        <HomePage />
+                    </Route>
+                </Switch>
+            </div>
+        </Container>
+    </Router>
+}
+
+const LanguageButton: React.FC<{ i18n: any, onSelect: () => void }> = ({ i18n, onSelect }) => {
+    const changeLanguage = (langKey: string) => {
+        i18n.changeLanguage(langKey);
+        onSelect();
+    }
+    const currentLanguage: string = i18n.language in LANGUAGE_MAP ? i18n.language : 'en';
+
+    return <NavDropdown
+        id="language-dropdown"
+        title={LANGUAGE_MAP[currentLanguage]}
+        menuVariant="dark"
+        className="language-dropdown"
+    >
+        <NavDropdown.Item onClick={() => changeLanguage('en')}>English</NavDropdown.Item>
+        <NavDropdown.Item onClick={() => changeLanguage('es')}>Espanol</NavDropdown.Item>
+    </NavDropdown>
 }
--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,37 +1,13 @@
 import React from 'react';
-import { Jumbotron } from '../components/Jumbotron';
+import { TextJumbotron } from '../components/Jumbotron';
 
 const AboutPage: React.FC = () => {
-  return <div>
-    <Jumbotron variant="light" title="Redistributing Wealth">
-      <p>
-        Trinity Mutual Aid is a student-run organization (unaffiliated with Trinity University) that
-        focuses on redistributing wealth within the San Antonio community. Our organization provides
-        aid to those in need of financial assistance with medical expenses, housing expenses, and food
-        insecurity. We do so in hopes of strengthening community connections so we can fulfill
-        collective needs together.
-      </p>
-    </Jumbotron>
-    <br />
-    <Jumbotron variant="dark" title="How we Started">
-      <p>
-        TMA was created in the fall of 2020 with a primary focus of combating food insecurity. Pantry
-        runs were our primary form of outreach. Our focus expanded in February of 2021, when the winter
-        storm that hit Texas created a huge need for aid. We provided aid to folks in need of assistance
-        with utility bills, medical bills, and more, as well as distributing essential
-        cold-weather items to houseless folks.
-      </p>
-    </Jumbotron>
-    <br />
-    <Jumbotron variant="light" title="A Wider Network of Aid">
-      <p>
-        We have affiliated with local non-profit Community Housing Resource Partners, an organization
-        that focuses on developing affordable housing to promote “self-sufficiency, education, health
-        and wellness, and stable communities.” This partnership ensures long-term sustainability in our
-        organization so that we can continue to provide aid within the community.
-      </p>
-    </Jumbotron>
-  </div>
+
+    return <div>
+        <TextJumbotron variant="light" i18nKey="pages.About.0" />
+        <TextJumbotron variant="dark" i18nKey="pages.About.1" />
+        <TextJumbotron variant="light" i18nKey="pages.About.2" />
+    </div>
 }
 
 export default AboutPage;

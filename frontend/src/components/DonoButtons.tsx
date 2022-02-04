7f96815dd580ac4a9e3e1bf14636d02fa8236ee9
Jordan Koeller <jkoeller12@gmail.com>
2022-02-04 00:43:30 -0600

    Added changes from Lexie's email on 2/3/22 to update contact us, remove the resources page, and fix some translations. Finished migrating dependencies to Node 16

--- frontend/src/components/DonoButtons.tsx
+++ frontend/src/components/DonoButtons.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import "./Donobuttons.css";
 
 export const SquareSingleDonoButton: React.FC = () => {
+    const { t } = useTranslation(undefined, { useSuspense: false });
 
     useEffect(() => {
         function showCheckoutWindow(e: Event) {
@@ -46,7 +48,7 @@ export const SquareSingleDonoButton: React.FC = () => {
                 <div id="renderedCheckoutButtonHTML" >
                     <div style={{ padding: '20px' }}>
                         <p style={{ fontSize: '18px', lineHeight: '20px' }}>
-                            Donate to Trinity Mutual Aid today!
+                            {t('pages.Donate.SquareSingle')}
                         </p>
                         <a
                             id="embedded-checkout-modal-checkout-button"
@@ -67,7 +69,7 @@ export const SquareSingleDonoButton: React.FC = () => {
                                 borderRadius: '50px'
                             }}
                         >
-                            Donate
+                            {t('pages.Donate.Donate')}
                         </a>
                     </div>
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Domine" />
@@ -78,6 +80,7 @@ export const SquareSingleDonoButton: React.FC = () => {
 }
 
 export const SquareRecurringDonoButton: React.FC = () => {
+    const { t } = useTranslation(undefined, { useSuspense: false });
 
     useEffect(() => {
         function showCheckoutWindow(e: Event) {
@@ -120,7 +123,7 @@ export const SquareRecurringDonoButton: React.FC = () => {
             <div ><div id="renderedCheckoutButtonHTML">
                 <div style={{ padding: '20px' }}>
                     <p style={{ fontSize: '18px', lineHeight: '20px' }}>
-                        Become a Reccurring Donor Today!
+                        {t('pages.Donate.SquareRecurring')}
                     </p>
                     <a id="embedded-checkout-modal-checkout-button" rel="noreferrer" target="_blank" data-url="https://square.link/u/rOj4d4oI?src=embd" href="https://square.link/u/rOj4d4oI?src=embed"
                         style={{
@@ -135,7 +138,10 @@ export const SquareRecurringDonoButton: React.FC = () => {
                             boxShadow: '0 0 0 1px rgba(0,0,0,.1) inset',
                             borderRadius: '50px'
                         }}
-                    >Donate</a>
+
+                    >
+                        {t('pages.Donate.Donate')}
+                    </a>
                 </div>
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Domine" />
 
@@ -147,6 +153,7 @@ export const SquareRecurringDonoButton: React.FC = () => {
 }
 
 export const PaypalDonoButton: React.FC = () => {
+    const { t } = useTranslation(undefined, { useSuspense: false });
     return (
         <form action="https://www.paypal.com/donate" method="post" target="_top">
             <input type="hidden" name="business" value="P5Y4NKNV99F6C" />

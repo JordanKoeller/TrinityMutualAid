07e10790abd74826f4c893bc135159c10611f042
Jordan Koeller <jkoeller12@gmail.com>
2022-02-10 22:01:20 -0600

    Adding aid request form with translations

--- frontend/src/pages/RequestAid.tsx
+++ frontend/src/pages/RequestAid.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import { useTranslation } from 'react-i18next';
+import { ExtLink } from '../components/ExtLink';
+import { TextJumbotron } from '../components/Jumbotron';
+import { TopSpacer } from '../components/TopSpacer';
+
 const RequestAidPage: React.FC = () => {
-    return <h1>
-        This is the Request aid page.
-    </h1>
+    const { t } = useTranslation(undefined, { useSuspense: false });
+    return <div>
+        <TopSpacer />
+        <TextJumbotron variant="light" i18nKey="pages.RequestAid.0" />
+        <TextJumbotron variant="light" i18nKey="pages.RequestAid.1" />
+        <h2>
+            {t('pages.RequestAid.linkLabel')} <ExtLink href="https://forms.gle/8iQgdBFAVeHNRRP66">
+                https://forms.gle/8iQgdBFAVeHNRRP66
+            </ExtLink>
+        </h2>
+        <TopSpacer />
+    </div>
 }
 
 export default RequestAidPage;

0220698c00d55be4b018cbed20fcb6c994905d94
Jordan Koeller <jkoeller12@gmail.com>
2022-04-02 18:56:16 -0500

    Fixed some styling junk on InfoCard

--- frontend/src/components/InfoCard.tsx
+++ frontend/src/components/InfoCard.tsx
@@ -12,49 +12,36 @@ export const InfoCard: React.FC<{
     href?: string
 }> = ({ title, imageUrl, link, children, href }) => {
     const sz = useMediaQuery();
+    const w = sz <= MediaQuery.SM ? '100%' : sz <= MediaQuery.MD ? '48%' : '30%';
     const cardStyle = {
         backgroundColor: "#3e5b47",
         fontSize: '5mm',
-        // flexBasis: '30%',
-        maxWidth: sz <= MediaQuery.MD ? undefined : '30%',
         marginTop: '5mm',
-        marginBottom: '5mm'
-        // marginLeft: '3mm',
-        // marginRight: '3mm'
+        marginBottom: '5mm',
+        maxWidth: w,
+
     };
     return <Card style={cardStyle}>
-        <ExtLink href={href}>
-            <Card.Body>
-                <Card.Title><h2>{title}</h2></Card.Title>
-                {
-                    imageUrl ? <Card.Img variant="top" src={imageUrl} /> : null
-                }
-                <Card.Text>
-                    <div style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'space-evenly',
-                        alignItems: 'baseline',
-                        alignContent: 'space-evenly',
-                        flexWrap: 'wrap',
-                    }}>
-                        {children}
-                    </div>
-                </Card.Text>
-                <Card.Link href="#">{link}</Card.Link>
-            </Card.Body>
-        </ExtLink>
-    </Card>
+            <ExtLink href={href}>
+                <Card.Body>
+                    <Card.Title><h2>{title}</h2></Card.Title>
+                    {
+                        imageUrl ? <Card.Img variant="top" src={imageUrl} width="100%" /> : null
+                    }
+                    <Card.Text>
+                            {children}
+                    </Card.Text>
+                    <Card.Link href="#">{link}</Card.Link>
+                </Card.Body>
+            </ExtLink>
+        </Card>
+
 };
 
 export const CardDeck: React.FC<{ fluid?: 'sm' | 'md' | 'lg' | true | false }> = ({ children, fluid = 'md' }) => {
-    const grouped = useMemo(() => groupBy(React.Children.toArray(children), 3), [children]);
     return <Container fluid={fluid}>
-        {
-            grouped.map(rowChildren =>
-                <Row style={{ justifyContent: 'space-around', columnGap: '5mm' }} md={1} lg={3}>
-                    {rowChildren}
-                </Row>)
-        }
+        <Row style={{ justifyContent: 'space-around', rowGap: '10mm'}} sm={1} md={2} lg={3}>
+            {children}
+        </Row>
     </Container>
 }
